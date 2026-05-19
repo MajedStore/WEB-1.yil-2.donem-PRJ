191dbdb990cb2dc7700798d27ea3753cf687738f
MajedStore <info.majedpro@gmail.com>
2026-05-19 22:18:21 +0300

    final changes

--- js/script.js
+++ js/script.js
@@ -1,43 +1,30 @@
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
 
-const menu=document.getElementById("menu");
-const toggle=document.querySelector(".nav-toggle");
-
-if(toggle){
-toggle.onclick=()=>menu.classList.toggle("open");
-}
-
-const cart=JSON.parse(localStorage.getItem("cart"))||[];
-
-document.querySelectorAll("[data-add]").forEach(btn=>{
-btn.onclick=()=>{
-cart.push({
-name:btn.dataset.name,
-price:btn.dataset.price
+document.querySelectorAll("[data-add]").forEach(function(btn){
+  btn.onclick=function(){
+    cart.push({
+      name:btn.dataset.name,
+      price:Number(btn.dataset.price)
+    });
+    localStorage.setItem("cart",JSON.stringify(cart));
+    alert("Urun sepete eklendi");
+  };
 });
 
-localStorage.setItem("cart",JSON.stringify(cart));
-
-alert("Urun sepete eklendi");
-};
-});
-
-const table=document.getElementById("cartTable");
-
+let table=document.getElementById("cartTable");
 if(table){
-let total=0;
-
-cart.forEach(item=>{
-total+=Number(item.price);
-
-table.innerHTML+=`
-<tr>
-<td>${item.name}</td>
-<td>${item.price} TL</td>
-</tr>
-`;
-});
-
-document.getElementById("total").innerText=total+" TL";
+  let total=0;
+  cart.forEach(function(item){
+    total+=item.price;
+    table.innerHTML+="<tr><td>"+item.name+"</td><td>"+item.price+" TL</td></tr>";
+  });
+  document.getElementById("total").innerHTML=total+" TL";
 }
 
-localStorage.removeItem("cart");
+let form=document.getElementById("contactForm");
+if(form){
+  form.onsubmit=function(e){
+    e.preventDefault();
+    document.getElementById("note").innerHTML="Mesaj alindi.";
+  };
+}
